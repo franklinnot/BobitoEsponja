869053adcf7a192f4d0c710a5588f431977854a0
franklinnot <franklinnot@outlook.com>
2024-11-01 13:37:57 -0500

    Método de semiesfera preparado para aplicar una textura

--- sem11_compugraph/sem11_compugraph/MetodosPoligonos.h
+++ sem11_compugraph/sem11_compugraph/MetodosPoligonos.h
@@ -199,32 +199,23 @@ void semiEsfera(float radius) {
     }
 }
 
-void semiEsfera(GLUquadric* quadcito, GLdouble radius) {
-    int slices = 100;
-    int stacks = 100;
-    for (int i = 0; i <= stacks / 2; ++i) {
-        float theta1 = (float)i / stacks * 3.1415;
-        float theta2 = (float)(i + 1) / stacks * 3.1415;
 
-        glBegin(GL_TRIANGLE_STRIP);
-        for (int j = 0; j <= slices; ++j) {
-            float phi = (float)j / slices * 2.0f * 3.1415;
+void semiEsfera(GLUquadric* quad, GLdouble radio) {
+    glPushMatrix();
+    glRotatef(180, 0.0, 0.0, 1.0);
 
-            float x1 = radius * sinf(theta1) * cosf(phi);
-            float y1 = radius * cosf(theta1);
-            float z1 = radius * sinf(theta1) * sinf(phi);
+    glEnable(GL_CLIP_PLANE0);
+    GLdouble equation[] = { 0.0, -1.0, 0.0, 0.0 }; 
+    glClipPlane(GL_CLIP_PLANE0, equation);
 
-            float x2 = radius * sinf(theta2) * cosf(phi);
-            float y2 = radius * cosf(theta2);
-            float z2 = radius * sinf(theta2) * sinf(phi);
+    quad = gluNewQuadric();
+    gluQuadricTexture(quad, 1);
+    gluSphere(quad, radio, 50, 50);
+    gluDeleteQuadric(quad);
 
-            glVertex3f(x1, y1, z1);  // Primer vértice
-            glVertex3f(x2, y2, z2);  // Segundo vértice
-        }
-        glEnd();
-    }
+    glDisable(GL_CLIP_PLANE0);
+    glPopMatrix();
 }
-
 #pragma endregion
 
 
--- sem11_compugraph/sem11_compugraph/main.cpp
+++ sem11_compugraph/sem11_compugraph/main.cpp
@@ -13,8 +13,8 @@ using namespace std;
 
 #pragma region Código de cajon
 
-float camaraX = -80;
-float camaraY = 90;
+float camaraX = 50;
+float camaraY = 75;
 float camaraZ = 50;
 float angulo = 0;
 GLuint texturas[100];
@@ -189,7 +189,7 @@ void dibujar() {
     glEnable(GL_BLEND);
     glLoadIdentity();
     // Linea para modificar el punto al cual la camara debe enfocar
-    gluLookAt(camaraX, camaraY, camaraZ, 0, 25, 0, 0, 1, 0);
+    gluLookAt(camaraX, camaraY, camaraZ, 0, 55, 0, 0, 1, 0);
     glClearColor(255 / 255.0, 255 / 255.0, 210 / 255.0, 1);
     glPushMatrix();
     glRotated(angulo, 0, 1, 0);
@@ -200,7 +200,7 @@ void dibujar() {
     // Aqui colocar todo el codigo
 
     WorkSpace(texturas);
-    
+    Isla(texturas, quad);
 
     // aqui termina
 
