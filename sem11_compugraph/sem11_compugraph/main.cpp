9b1d28670c6070b60f28d23cb378c760975ac73e
alxtorres6 <alxtorres6@gmail.com>
2024-11-04 16:58:29 -0500

    camara
    Cambio de camara

--- sem11_compugraph/sem11_compugraph/main.cpp
+++ sem11_compugraph/sem11_compugraph/main.cpp
@@ -14,9 +14,9 @@ using namespace std;
 
 #pragma region Código de cajon
 
-float camaraX = 50;
-float camaraY = 75;
-float camaraZ = 50;
+float camaraX = -60;
+float camaraY = 20;
+float camaraZ = 60;
 float angulo = 0;
 GLuint texturas[100];
 GLUquadric* quad;
@@ -479,8 +479,9 @@ void dibujar() {
     glEnable(GL_BLEND);
     glLoadIdentity();
     // Linea para modificar el punto al cual la camara debe enfocar
-    gluLookAt(camaraX, camaraY, camaraZ, 0, 55, 0, 0, 1, 0);
+    gluLookAt(camaraX, camaraY, camaraZ, 0, 0, 0, 0, 1, 0);
     glClearColor(255 / 255.0, 255 / 255.0, 210 / 255.0, 1);
+
     glPushMatrix();
     glRotated(angulo, 0, 1, 0);
     piso();
